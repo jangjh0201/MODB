3420be72ec63a5b1ea5bcc438323ce40dd72b3d1
jangjh0201 <jangjh0201@gmail.com>
2022-06-30 12:25:42 +0900

    test : 초기 테스트 코드 작성

--- src/test/java/com/dodatabase/movie_backend/MovieBackendApplicationTests.java
+++ src/test/java/com/dodatabase/movie_backend/MovieBackendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.dodatabase.movie_backend;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dodatabase.movie_backend.service.ApiKey;
 
 @SpringBootTest
 class MovieBackendApplicationTests {
 
+    @Autowired
+    ApiKey apiKey;
+
     @Test
-    void contextLoads() {
+    public void test() {
+        assertThat(apiKey.getId(), is(equalTo("Hqsz1tECcg712EE903wl")));
     }
 
 }
