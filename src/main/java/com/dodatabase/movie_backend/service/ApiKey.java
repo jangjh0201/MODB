d2daafe340d93a1bcb018c5a779d766859896ffd
jangjh0201 <jangjh0201@gmail.com>
2023-01-09 03:21:46 +0900

    feat : add new annotations

--- src/main/java/com/dodatabase/movie_backend/service/ApiKey.java
+++ src/main/java/com/dodatabase/movie_backend/service/ApiKey.java
@@ -1,19 +1,31 @@
 package com.dodatabase.movie_backend.service;
 
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "movie-api")
 @ConstructorBinding
 public class ApiKey {
 
-    private final String Url;
-    private final String Id;
-    private final String Secret;
+    @NotBlank
+    private final String id;
+
+    @NotBlank
+    private final String secret;
+
+    @Builder
+    public ApiKey(String id, String secret) {
+        this.id = id;
+        this.secret = secret;
+    }
 
 }
--- src/main/java/com/dodatabase/movie_backend/service/MovieService.java
+++ src/main/java/com/dodatabase/movie_backend/service/MovieService.java
@@ -3,9 +3,9 @@ package com.dodatabase.movie_backend.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.dodatabase.movie_backend.domain.Movie;
+import com.dodatabase.movie_backend.domain.Movie.Movie;
 import com.dodatabase.movie_backend.repository.MovieRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +17,7 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
+    @Transactional
     public void create(Movie movie) {
         movieRepository.save(movie);
     }
@@ -24,16 +25,19 @@ public class MovieService {
     /**
      * 전체 도서 목록 조회
      */
+    @Transactional(readOnly = true)
     public List<Movie> findMovies() {
         return movieRepository.findAll();
     }
 
-    @Transactional()
+    @Transactional(readOnly = true)
+    public Optional<Movie> findByTitle(String keyword) {
+        return movieRepository.findByTitle(keyword);
+    }
+
+    @Transactional
     public void deleteByTitle(String title) {
         movieRepository.deleteByTitle(title);
     }
 
-    public Optional<Movie> findByTitle(String keyword) {
-        return movieRepository.findByTitle(keyword);
-    }
 }
