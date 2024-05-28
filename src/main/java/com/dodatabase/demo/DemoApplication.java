e4bd9175bcf3ceda3e3e83a35294d5206f3ad994
jangjh0201 <jangjh0201@gmail.com>
2024-05-28 23:58:32 +0900

    ✏️ fix typo

--- src/main/java/com/dodatabase/demo/DemoApplication.java
+++ src/main/java/com/dodatabase/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * SpringBoot 어플리케이션 클래스.
+ */
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class DemoApplication {
