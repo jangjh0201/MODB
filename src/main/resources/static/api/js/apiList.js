e99a3b776cef3d3094c91aa98a50aac237037071
jangjh0201 <jangjh0201@gmail.com>
2024-05-29 23:48:33 +0900

    ✏️ Fix typo

--- src/main/resources/static/api/js/apiList.js
+++ src/main/resources/static/api/js/apiList.js
@@ -10,27 +10,27 @@ function errorCheck() {
 // 영화 저장 버튼 클릭 이벤트 처리
 $(document).ready(function () {
   $(".save-button").on("click", function () {
-    var movieId = $(this).data("movie-id");
-    saveMovie(movieId);
+    var movieIndex = $(this).data("movie-index");
+    saveMovie(movieIndex);
   });
 });
 
 // 영화 저장 함수
-function saveMovie(movieId) {
+function saveMovie(movieIndex) {
   $.ajax({
     type: "POST",
     url: "/movie/new",
-    data: JSON.stringify(movieId),
     dataType: "text",
     contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(movieIndex),
   })
     .done(() => {
       alert("등록되었습니다.");
     })
     .fail((jqXHR) => {
-      if (jqXHR.status === 409) {
+      if (jqXHR.status == 409) {
         alert("이미 등록된 영화입니다.");
-      } else if (jqXHR.status === 404) {
+      } else if (jqXHR.status == 404) {
         alert("영화를 찾을 수 없습니다.");
       }
     });
