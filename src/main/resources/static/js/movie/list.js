541fc986171565ca2c1c525084024a4cad4a5ccb
jangjh0201 <jangjh0201@gmail.com>
2024-06-05 23:29:27 +0900

    💄 Rename UI files
    ref : fe75da2bf29cc984b60904a08ee25381fee3366a

--- src/main/resources/static/js/movie/list.js
+++ src/main/resources/static/js/movie/list.js
@@ -1,27 +1,37 @@
-// 영화 삭제 버튼 클릭 이벤트 처리
+function errorCheck() {
+  if (document.searchForm.title.value == "") {
+    alert("검색어는 필수입니다.");
+    return false;
+  } else {
+    document.searchForm.submit(); // 폼을 제출합니다.
+  }
+}
+
+// 영화 저장 버튼 클릭 이벤트 처리
 $(document).ready(function () {
-  $(".delete-button").on("click", function () {
-    var movieId = $(this).data("movie-id");
-    deleteMovie(movieId);
+  $(".save-button").on("click", function () {
+    var movieIndex = $(this).data("movie-index");
+    saveMovie(movieIndex);
   });
 });
 
-// 영화 삭제 함수
-function deleteMovie(movieId) {
+// 영화 저장 함수
+function saveMovie(movieIndex) {
   $.ajax({
-    type: "DELETE",
-    url: "/v1/movie/delete",
+    type: "POST",
+    url: "/v1/favorites",
     dataType: "text",
     contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(movieId),
+    data: JSON.stringify(movieIndex),
   })
     .done(() => {
-      alert("삭제되었습니다.");
-      location.reload();
+      alert("등록되었습니다.");
     })
     .fail((jqXHR) => {
-      if (jqXHR.status == 400) {
-        alert("영화를 삭제할 수 없습니다.");
+      if (jqXHR.status == 409) {
+        alert("이미 등록된 영화입니다.");
+      } else if (jqXHR.status == 404) {
+        alert("영화를 찾을 수 없습니다.");
       }
     });
 }
