8749d98897819fc13f42b12ba12f17f494823294
jangjh0201 <jangjh0201@gmail.com>
2024-06-15 22:55:01 +0900

    ♻️ Refactor api response domain

--- src/main/java/com/dodatabase/demo/domain/movie/MovieResponse.java
+++ src/main/java/com/dodatabase/demo/domain/movie/MovieResponse.java
@@ -1,122 +1,33 @@
 package com.dodatabase.demo.domain.movie;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class MovieResponse {
-  private String query;
-  private String kmaQuery;
-  private long totalCount;
-  private List<Data> data;
+  private String id;
+  private String title;
+  private int prodYear;
+  private String genre;
+  private String nation;
+  private int runtime;
+  private String director;
+  private String actor;
 
-  @Builder
-  private MovieResponse(String query, String kmaQuery, long totalCount, List<Data> data) {
-    this.query = query;
-    this.kmaQuery = kmaQuery;
-    this.totalCount = totalCount;
-    this.data = data;
+  @Builder(builderClassName = "MovieResponseBuilder", builderMethodName = "movieResponseBuilder")
+  public MovieResponse(String id, String title, int prodYear, String genre,
+      String nation, int runtime, String director, String actor) {
+    this.id = id;
+    this.title = title;
+    this.prodYear = prodYear;
+    this.genre = genre;
+    this.nation = nation;
+    this.runtime = runtime;
+    this.director = director;
+    this.actor = actor;
   }
 
-  @Getter
-  @NoArgsConstructor
-  public static class Data {
-    private String collName;
-    private long totalCount;
-    private long count;
-    private List<Result> result;
-
-    @Builder
-    private Data(String collName, long totalCount, long count, List<Result> result) {
-      this.collName = collName;
-      this.totalCount = totalCount;
-      this.count = count;
-      this.result = result;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class Result {
-      @JsonProperty("DOCID")
-      private String docId;
-      private String title;
-      private String prodYear;
-      private String genre;
-      private String nation;
-      private String runtime;
-      private Directors directors;
-      private Actors actors;
-
-      @Builder
-      private Result(String docId, String title, String prodYear, String genre,
-          String nation, String runtime, Directors directors, Actors actors) {
-        this.docId = docId;
-        this.title = title;
-        this.prodYear = prodYear;
-        this.genre = genre;
-        this.nation = nation;
-        this.runtime = runtime;
-        this.directors = directors;
-        this.actors = actors;
-      }
-
-      @Getter
-      @NoArgsConstructor
-      public static class Directors {
-        @JsonProperty("director")
-        private List<Director> directors;
-
-        @Builder
-        private Directors(List<Director> directors) {
-          this.directors = directors;
-        }
-
-        @Getter
-        @NoArgsConstructor
-        public static class Director {
-          private String directorNm;
-          private String directorEnNm;
-          private String directorId;
-
-          @Builder
-          private Director(String directorNm, String directorEnNm, String directorId) {
-            this.directorNm = directorNm;
-            this.directorEnNm = directorEnNm;
-            this.directorId = directorId;
-          }
-        }
-      }
-
-      @Getter
-      @NoArgsConstructor
-      public static class Actors {
-        @JsonProperty("actor")
-        private List<Actor> actors;
-
-        @Builder
-        public Actors(List<Actor> actors) {
-          this.actors = actors;
-        }
-
-        @Getter
-        @NoArgsConstructor
-        public static class Actor {
-          private String actorNm;
-          private String actorEnNm;
-          private String actorId;
-
-          @Builder
-          public Actor(String actorNm, String actorEnNm, String actorId) {
-            this.actorNm = actorNm;
-            this.actorEnNm = actorEnNm;
-            this.actorId = actorId;
-          }
-        }
-      }
-    }
+  public static MovieResponseBuilder builder() {
+    return movieResponseBuilder();
   }
 }
