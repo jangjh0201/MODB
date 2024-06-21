cd338e6de1760d2cb7bc0eb33d4f9cd5bddcf50d
jangjh0201 <jangjh0201@gmail.com>
2024-06-22 02:12:31 +0900

    🔥 Remove Jsonproperty

--- src/main/java/com/dodatabase/demo/domain/wishlist/WishRequest.java
+++ src/main/java/com/dodatabase/demo/domain/wishlist/WishRequest.java
@@ -1,15 +1,9 @@
 package com.dodatabase.demo.domain.wishlist;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WishRequest {
   private String id;
   private String title;
@@ -21,15 +15,8 @@ public class WishRequest {
   private String actor;
 
   @Builder(builderClassName = "WishRequestBuilder", builderMethodName = "wishRequestBuilder")
-  @JsonCreator
-  public WishRequest(@JsonProperty("id") String id,
-      @JsonProperty("title") String title,
-      @JsonProperty("prodYear") int prodYear,
-      @JsonProperty("genre") String genre,
-      @JsonProperty("nation") String nation,
-      @JsonProperty("runtime") int runtime,
-      @JsonProperty("director") String director,
-      @JsonProperty("actor") String actor) {
+  public WishRequest(String id, String title, int prodYear, String genre,
+      String nation, int runtime, String director, String actor) {
     this.id = id;
     this.title = title;
     this.prodYear = prodYear;
