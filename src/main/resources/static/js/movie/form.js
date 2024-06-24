e8d4b5f35fd629293d637d5ec82c0b74e0005038
jangjh0201 <jangjh0201@gmail.com>
2024-06-24 23:46:48 +0900

    ⚰️ Remove unused code
    ref : 709598958e7e3521a54f2b420c86553d7feca08e

--- src/main/resources/static/js/movie/form.js
+++
@@ -1,8 +0,0 @@
-function errorCheck() {
-  if (document.searchForm.title.value == "") {
-    alert("검색어는 필수입니다.");
-    return false;
-  } else {
-    document.searchForm.submit(); // 폼을 제출합니다.
-  }
-}
