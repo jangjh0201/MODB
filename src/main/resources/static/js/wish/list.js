1724faa742864cad138d8b81019e775ebe3a461d
jangjh0201 <jangjh0201@gmail.com>
2024-06-25 23:59:21 +0900

    🚧 WORK IN PROGRESS

--- src/main/resources/static/js/wish/list.js
+++ src/main/resources/static/js/wish/list.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/v1/wish")
-    .then((response) => response.json())
-    .then((data) => {
-      renderWishes(data);
-    })
-    .catch((error) => console.error("Error:", error));
+  fetchWishes(); // 위시리스트를 가져오는 함수 호출
 });
 
-function renderWishes(movies) {
+function fetchWishes() {
+  fetch("/v1/wish")
+    .then((response) => response.json())
+    .then((data) => renderWishes(data))
+    .catch((error) => console.error("Error:", error));
+}
+
+function renderWishes(wishes) {
   const container = document.getElementById("wishList");
   container.innerHTML = ""; // Clear previous results
 
-  if (movies.length === 0) {
-    container.innerHTML =
-      "<p class='text-center no-movies-message'>저장된 영화가 없습니다.</p>";
+  if (wishes.length === 0) {
+    container.innerHTML = "<p class='text-center'>저장된 영화가 없습니다.</p>";
     return;
   }
 
@@ -37,45 +38,41 @@ function renderWishes(movies) {
   const tbody = document.createElement("tbody");
   tbody.classList.add("table-group-divider");
 
-  movies.forEach((movie) => {
+  wishes.forEach((wish) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `
-      <td>${movie.title}</td>
-      <td>${movie.prodYear}</td>
-      <td>${movie.genre}</td>
-      <td>${movie.nation}</td>
-      <td>${movie.runtime}</td>
-      <td>${movie.director}</td>
-      <td class="fixed-width-td">${movie.actor}</td>
+      <td>${wish.title}</td>
+      <td>${wish.prodYear}</td>
+      <td>${wish.genre}</td>
+      <td>${wish.nation}</td>
+      <td>${wish.runtime}</td>
+      <td>${wish.director}</td>
+      <td class="fixed-width-td">${wish.actor}</td>
       <td>
-        <button class="btn btn-outline-primary delete-button" data-movie-id="${movie.id}">삭제</button>
+        <button class="btn btn-outline-primary delete-button" data-movie-id="${wish.id}">삭제</button>
       </td>`;
     tbody.appendChild(tr);
+
+    tr.querySelector(".delete-button").addEventListener("click", function () {
+      deleteWish(wish.id);
+    });
   });
 
   table.appendChild(tbody);
   container.appendChild(table);
-
-  // Delete button event listener
-  document.querySelectorAll(".delete-button").forEach((button) => {
-    button.addEventListener("click", function () {
-      deleteMovie(this.dataset.movieId);
-    });
-  });
 }
 
-function deleteMovie(movieId) {
-  fetch("/v1/wish", {
+function deleteWish(wishId) {
+  fetch(`/v1/wish/${wishId}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ id: movieId }),
   })
     .then((response) => {
       if (response.status === 200) {
         alert("삭제되었습니다.");
-        location.reload(); // Reload the wish list
+        fetchWishes(); // Wish list refresh
       } else {
         alert("오류가 발생했습니다.");
       }
